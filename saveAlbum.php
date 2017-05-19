27661e7f70a845798525c024eb62acfdfbc3715f
JaretWright <hkkA223>
2017-05-19 10:33:19 -0400

    added DB commands

--- saveAlbum.php
+++ saveAlbum.php
@@ -14,6 +14,26 @@
     echo 'title: '.$title.'<br />';
     echo 'year: '.$year.'<br />';
     echo 'artist: '.$artist.'<br />';
+
+    //step 1 - connect to the database
+    $conn = new PDO('mysql:host=aws.computerstudi.es;dbname=gc500182472',
+                'gc500182472', 'EInCI-ksZ_');
+
+    //step 2 - create the SQL command to INSERT a record
+    $sql = "INSERT INTO albums (title, year, artist) VALUES (:title, :year, :artist);";
+
+
+    //step 3 - prepare the SQL command and bind the arguments to prevent SQL injection
+    $cmd = $conn->prepare($sql);
+    $cmd->bindParam(':title', $title, PDO::PARAM_STR, 50);
+    $cmd->bindParam(':year', $year, PDO::PARAM_INT, 4);
+    $cmd->bindParam(':artist', $artist, PDO::PARAM_STR, 50);
+
+
+//step 4 - execute
+    //step 5 - disconnect from database
+    //step 6 - redirect to the albums page
+
 ?>
 </body>
 
