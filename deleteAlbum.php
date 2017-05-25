e5e131d1f511a913a96b6d419518d9efcb40a0ce
JaretWright <jaret.wright@georgiancollege.ca>
2017-05-25 10:23:40 -0400

    added deleteAlbum.php

--- deleteAlbum.php
+++ deleteAlbum.php
@@ -1,3 +1,5 @@
+<?php ob_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +7,25 @@
     <title>Deleting Album</title>
 </head>
 <body>
-    
+<?php
+    //Step 1 - connect to the DB
+    $conn = new PDO('mysql:host=localhost;dbname=php','root','admin');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Step 2 - Create SQL query
+    $sql = "DELETE FROM albums WHERE albumID = :albumID";
+
+    //Step 3 - prepare & execute the SQL
+    $cmd = $conn->prepare($sql);
+    $cmd->bindParam(':albumID', $_GET['albumID'], PDO::PARAM_INT);
+    $cmd->execute();
+
+    //Step 4 - disconnect from the DB
+    $conn=null;
+
+    //Step 5 - redirect to the albums.php page
+    header('location:albums.php');
+?>
 </body>
 </html>
+<?php ob_flush(); ?>
